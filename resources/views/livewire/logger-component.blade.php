4f6923977aad56a7230d2116b78b2e0c0794efed
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-01-31 14:30:07 +0700

    add button action in logger component

--- resources/views/livewire/logger-component.blade.php
+++ resources/views/livewire/logger-component.blade.php
@@ -77,7 +77,12 @@
                             {{$logger->area}}
                         </td>
                         <td class="px-6 py-4">
-                            $2999
+                            <a href="">
+                                <button type="button" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:focus:ring-yellow-900">Edit</button>
+                            </a>
+                            <a href="">
+                                <button type="button" class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>   
+                            </a>
                         </td>
                     </tr>
                 @endforeach
