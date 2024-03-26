be8843bda4228958e757695c31d2aa564d9d1395
RayhanFurqon <rayhanfurqon99@gmail.com>
2024-03-26 12:02:24 +0700

    Add new migration

---
+++ database/migrations/2024_03_26_035538_create_devices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->string('serial_number')->primary();
+            $table->string('name');
+            $table->string('code');
+            $table->point('last_location');
+            $table->json('last_monitored_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
---
+++ database/migrations/2024_03_26_040126_create_measurements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('measurements', function (Blueprint $table) {
+            $table->string('device_id')->primary();
+            $table->foreign('device_id')->references('serial_number')->on('devices');
+            $table->dateTime('datetime');
+            $table->string('key');
+            $table->integer('value');
+            $table->string('unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('measurements');
+    }
+};
