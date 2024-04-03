6740827b06071defd2302bd48589fde952ee5f03
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-04-03 13:04:55 +0700

    Integration stations data to maps

--- resources/views/livewire/maps-component.blade.php
+++ resources/views/livewire/maps-component.blade.php
@@ -1,15 +1,25 @@
 <div class="container-fluid">
         <div id="map" class="lg:mt-[100px] mt-[50px] lg:w-[2000px] w-full"></div>
-        {{-- <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script> --}}
-        <script src="{{ asset('plugins/leaflet/leaflet.js') }}"></script>
-        <script src="{{ asset('plugins/leaflet/L.KML.js')}}"></script>
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
         <script>
-            map = L.map('map').setView([-4.198779206119091, 122.29329165271804], 5);
-        
-            tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            const stations = @json($stations);
+            
+            var map = L.map('map').setView([-4.628757657193269, 122.33816943962901], 5);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(map);
-        </script>
+            stations.forEach(station => {
+                var lat = station.latitude;
+                var long = station.longitude;
+                var customIcon = L.icon({
+                    iconUrl: '{{ URL::asset('img/railway.svg') }}',
+                    iconSize: [38, 95],
+                    iconAnchor: [22, 94],
+                    popupAnchor: [-3, -76]
+                });
+                L.marker([lat, long], {icon: customIcon}).addTo(map);
+            });
 
+        </script>
 </div>
