de3c7bcc9fc188d0fe95b61bf67600114ab41d08
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-04-04 10:56:01 +0700

    change marker name for station

--- resources/views/livewire/maps-component.blade.php
+++ resources/views/livewire/maps-component.blade.php
@@ -1,25 +1,40 @@
 <div class="container-fluid">
-        <div id="map" class="lg:mt-[100px] mt-[50px] lg:w-[2000px] w-full"></div>
-        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-        <script>
-            const stations = @json($stations);
-            
-            var map = L.map('map').setView([-4.628757657193269, 122.33816943962901], 5);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 19,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
-            stations.forEach(station => {
-                var lat = station.latitude;
-                var long = station.longitude;
-                var customIcon = L.icon({
-                    iconUrl: '{{ URL::asset('img/railway.svg') }}',
-                    iconSize: [38, 95],
-                    iconAnchor: [22, 94],
-                    popupAnchor: [-3, -76]
-                });
-                L.marker([lat, long], {icon: customIcon}).addTo(map);
-            });
+    <div id="map" class="lg:mt-[100px] mt-[50px] lg:w-[2000px] w-full"></div>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <script>
+        const stations = @json($stations);
+        const devices = @json($devices);
+        
+        var map = L.map('map').setView([-4.628757657193269, 122.33816943962901], 5);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
 
-        </script>
+        stations.forEach(station => {
+            var lat = station.latitude;
+            var long = station.longitude;
+            var stationIcon = L.icon({
+                iconUrl: '{{ URL::asset('img/railway.svg') }}',
+                iconSize: [50, 95],
+                iconAnchor: [22, 94],
+                popupAnchor: [-3, -76]
+            });
+            L.marker([lat, long], {icon:stationIcon}).addTo(map);
+        });
+
+        devices.forEach(device => {
+            var device_lat = device.latitude;
+            var device_long = device.longitude;
+            var deviceIcon = L.icon({
+                iconUrl: '{{ URL::asset('img/train.svg') }}',
+                iconSize: [50, 95],
+                iconAnchor: [22, 94],
+                popupAnchor: [-3, -76]
+            });
+            L.marker([device_lat, device_long], {icon: deviceIcon}).addTo(map);
+        });
+
+
+    </script>
 </div>
