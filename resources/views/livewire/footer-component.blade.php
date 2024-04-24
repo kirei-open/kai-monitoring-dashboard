c206da9cd6e09c3095ab1a677200c297a49337f6
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-04-24 14:35:58 +0700

    Change footer layout

--- resources/views/livewire/footer-component.blade.php
+++ resources/views/livewire/footer-component.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 lg:w-[105%] lg:ml-[-5px] lg:mt-[300px]">
+<footer class="fixed bg-white rounded-lg shadow m-4 dark:bg-gray-800 lg:w-[105%] w-[120%] lg:ml-[-15px] ml-[-10px] inset-x-2 bottom-[-20px]">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             &copy; {{ date('Y') }} KIREI. All Rights Reserved.
