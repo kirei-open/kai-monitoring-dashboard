342cb7f293889b4aad4d6cb55a0ba144ac8132de
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-04-30 11:57:31 +0700

    add detail button in pop up train

--- resources/views/livewire/maps-component.blade.php
+++ resources/views/livewire/maps-component.blade.php
@@ -2,6 +2,7 @@
     <div id="map" class="lg:mt-[100px] mt-[50px] lg:w-[2000px] w-full"></div>
     <script src="{{ URL::asset('js/leaflet.js') }}"></script>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         const stations = @json($stations);
         const devices = @json($devices);
@@ -18,7 +19,6 @@
         stations.forEach(station => {
             var lat = station.latitude;
             var long = station.longitude;
-            console.log(lat,long);
             
             var stationIcon = L.icon({
                 iconUrl: '{{ URL::asset('img/railway.svg') }}',
@@ -52,24 +52,17 @@
                 iconAnchor: [22, 94],
                 popupAnchor: [-3, -76]
             });
-    
+            
             var popupContent = `
                 <b>Serial Number:</b> ${device.serial_number}<br>
                 <b>Name:</b> ${device.name}<br>
                 <b>Code:</b> ${device.code}<br>
                 <b>Last Location:</b> ${latitude},${longitude}<br>
+                <a href="/table/detail/${device.serial_number}">Detail</a>
             `;
     
             var marker = L.marker([latitude, longitude], { icon: deviceIcon, device_id: device.serial_number }).bindPopup(popupContent);
             marker.addTo(markerLayerGroup);
-
-            marker.on('mouseover', function(e) {
-                this.bindPopup(popupContent).openPopup();
-            });
-            
-            marker.on('mouseout', function(e) {
-                this.closePopup();
-            });
         });
 
         function addOrUpdateMarkerFromBroadcast(device) {
@@ -83,6 +76,7 @@
             var popupContent = `
                 <b>Serial Number:</b> ${device.device_id}<br>
                 <b>Last Location:</b> ${device.latitude},${device.longitude}<br>
+                <a href="/table/detail/${device.device_id}">Detail</a>
             `;
             
             var existingMarker = markerLayerGroup.getLayers().find(marker => marker.options.device_id === device.device_id);
@@ -94,23 +88,14 @@
                 var newMarker = L.marker([device.longitude, device.latitude], { icon: deviceIcon, device_id: device.device_id }).bindPopup(popupContent);
                 newMarker.addTo(markerLayerGroup);
             }
-                newMarker.on('mouseover', function(e) {
-                this.bindPopup(popupContent).openPopup();
-            });
-            newMarker.on('mouseout', function(e) {
-                this.closePopup();
-            });
-            console.log('Marker updated or added:', device.device_id, device.latitude, device.longitude);
         }
     
         window.onload = function() {
             window.Echo.channel('location-channel')
-                .listen('DeviceLocationBroadcast', (data) => {
+            .listen('DeviceLocationBroadcast', (data) => {
                     var device = data.data;
-                    console.log(device);
                     addOrUpdateMarkerFromBroadcast(device);
                 });
         };
     </script>
 </div>
-
