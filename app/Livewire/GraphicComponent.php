6ab1d27db916731b8cece1848c89bc30a0b818fe
RayhanFurqon <rayhanfurqon99@gmail.com>
2024-05-02 09:51:03 +0700

    Update get list from device table

--- app/Livewire/GraphicComponent.php
+++ app/Livewire/GraphicComponent.php
@@ -2,14 +2,14 @@
 
 namespace App\Livewire;
 
-use App\Models\Measurement;
+use App\Models\Device;
 use Livewire\Component;
 
 class GraphicComponent extends Component
 {
     public function render()
     {
-        $device_id = Measurement::groupBy('device_id')->pluck('device_id', 'device_id');
+        $device_id = Device::groupBy('serial_number')->pluck('serial_number', 'serial_number');
         return view('livewire.graphic-component',['device_id' => $device_id]);
     }
 }
