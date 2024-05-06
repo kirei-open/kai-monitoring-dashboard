1407d91ad994184ba741d4e97e6853b16b0c2339
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-05-06 15:48:39 +0700

    Update Component

--- app/Livewire/GraphicComponent.php
+++ app/Livewire/GraphicComponent.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Device;
+use App\Models\Measurement;
 use Livewire\Component;
 
 class GraphicComponent extends Component
@@ -10,6 +11,7 @@ class GraphicComponent extends Component
     public function render()
     {
         $device_id = Device::groupBy('serial_number')->pluck('serial_number', 'serial_number');
-        return view('livewire.graphic-component',['device_id' => $device_id]);
+        $measurement = Measurement::all();
+        return view('livewire.graphic-component',['device_id' => $device_id, 'measurement' => $measurement]);
     }
 }
