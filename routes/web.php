986ce8ab4885ef850205a72ca2de15c320a5f03e
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-07-17 14:55:17 +0700

    bug fix graphic historical component

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MeasurementController;
 use App\Livewire\Pages\Dashboard\Dashboard;
 use App\Livewire\Pages\Graphic\Graphic;
 use App\Livewire\Pages\Table\Table;
@@ -25,11 +26,12 @@ Route::group(['middleware' => ['role:super_admin|Admin']],function(){
     Route::get('/dashboard',Dashboard::class)->name('dashboard');
     Route::get('/table',Table::class)->name('table');
     Route::get('/table/detail/{id}',TableDetail::class)->name('table.detail');
-    Route::get('/graphic',Graphic::class)->name('graphic');
 });
 
 Route::group(['middleware' => ['role:super_admin|Admin|Teknisi']],function(){
     Route::get('/graphic',Graphic::class)->name('graphic');
+    Route::get('/get-detail-measurement/{device_id}', [MeasurementController::class, 'getDetailMeasurement']);
+    Route::get('/get-last-thirty-minutes/{device_id}', [MeasurementController::class, 'getLastThirtyMinutesData']);
 });
 
 require __DIR__.'/auth.php';
