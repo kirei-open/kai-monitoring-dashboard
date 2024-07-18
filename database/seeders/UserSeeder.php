8559d9f6da10c3e81d1a8b43d4850a875a0c0bd7
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-07-18 10:53:27 +0700

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,33 +15,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        if (!User::where('email', 'superadmin@kai.co.id')->exists()) {
-            $super_admin = User::create([
+        if (!User::where('email', 'superadmin@mail.com')->exists()) {
+            User::create([
                 'name' => 'super_admin',
-                'email' => 'superadmin@kai.co.id',
+                'email' => 'superadmin@mail.com',
                 'password' => Hash::make('superadmin123')
             ]);
-            $super_admin->assignRole('super_admin');
         }
 
-        // Repeat the same check for other users
-
-        if (!User::where('email', 'admin@kai.co.id')->exists()) {
-            $Admin = User::create([
-                'name' => 'Admin',
-                'email' => 'admin@kai.co.id',
-                'password' => Hash::make('admin123')
-            ]);
-            $Admin->assignRole('Admin');
-        }
-
-        if (!User::where('email', 'teknisi@kai.co.id')->exists()) {
-            $Teknisi = User::create([
+        if (!User::where('email', 'teknisi@mail.com')->exists()) {
+            User::create([
                 'name' => 'Teknisi',
-                'email' => 'teknisi@kai.co.id',
+                'email' => 'teknisi@mail.com',
                 'password' => Hash::make('teknisi123')
             ]);
-            $Teknisi->assignRole('Teknisi');
         }
+
+        
     }
 }
