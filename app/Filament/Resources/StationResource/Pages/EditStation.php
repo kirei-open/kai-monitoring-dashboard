94728fa73281c05de2216ce1a73ebefa8ec6e8c1
RayhanFurqon <rayhanfurqon99@gmail.com>
2024-08-02 11:16:57 +0700

    Bug fix update stations on filament

--- app/Filament/Resources/StationResource/Pages/EditStation.php
+++ app/Filament/Resources/StationResource/Pages/EditStation.php
@@ -2,13 +2,21 @@
 
 namespace App\Filament\Resources\StationResource\Pages;
 
-use App\Filament\Resources\StationResource;
 use Filament\Actions;
+use Illuminate\Support\Facades\DB;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\StationResource;
 
 class EditStation extends EditRecord
 {
     protected static string $resource = StationResource::class;
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        $data['point'] = DB::raw("ST_GeomFromText('POINT({$data['longitude']} {$data['latitude']})')");
+
+        return $data;
+    }
 
     protected function getHeaderActions(): array
     {
