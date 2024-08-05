595c3702e9135c3d5517c6a50a7e3f2069e00110
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-08-05 08:35:30 +0700

    Add button for generate report

--- resources/views/livewire/pages/table/table-detail.blade.php
+++ resources/views/livewire/pages/table/table-detail.blade.php
@@ -1,11 +1,74 @@
 <div>
   @section('title', $device->serial_number)
-  <section class="bg-white dark:bg-gray-900 lg:mt-16">
+  <section class="lg:mt-[7rem] lg:ml-[325px]">
+    <button wire:click="generatePdf" type="button" class="text-white bg-blue-700 
+      hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
+      font-medium rounded-lg text-sm px-5 py-2.5 
+      me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
+      focus:outline-none dark:focus:ring-blue-800">
+      Generate Report
+    </button>
+  </section>
+  {{-- <section class="lg:mt-[7rem]">
+    <!-- Modal toggle -->
+    <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+      Toggle modal
+    </button>
+
+    <!-- Main modal -->
+    <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        Sign in to our platform
+                    </h3>
+                    <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-4 md:p-5">
+                    <form class="space-y-4" action="#">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                        </div>
+                        <div class="flex justify-between">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                                </div>
+                                <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+                            </div>
+                            <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                        </div>
+                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+                            Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> 
+
+  </section> --}}
+  <section class="bg-white dark:bg-gray-900 lg:mt-[-50px]">
     <div class="pt-8 px-4 mx-auto max-w-screen-xl lg:pt-16">
       <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
         <span
-          class="bg-blue-100 text-purple-800 text-md font-medium inline-flex items-center px-2.5 py-0.5 
-              rounded-md dark:bg-gray-700 dark:text-purple-400 mb-2">
+          class="bg-indigo-100 text-indigo-800 text-md font-medium inline-flex items-center px-2.5 py-0.5 
+              rounded-md dark:bg-indigo-900 dark:text-indigo-400 mb-2">
           Train
         </span>
         <div class="relative overflow-x-auto lg:mt-4">
@@ -35,8 +98,8 @@
                   {{ $train->name }}
                 </td>
                 <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  <img src="{{ asset('storage/' . $train->image) }}" alt="{{ $train->name }}"
-                    class="h-16 w-16 object-cover">
+                  <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path("storage/$train->image"))) }}"
+                      width="100" />
                 </td>
                 <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                   {{ $train->stations->pluck('name')->join(', ') }}
@@ -172,16 +235,6 @@
             @endforeach
           </tbody>
         </table>
-        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 lg:w-[1150px]"
-          aria-label="Table navigation">
-          <span
-            class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-            Showing <span class="font-semibold text-gray-900 dark:text-white">{{ $locations->firstItem() }}</span> -
-            <span class="font-semibold text-gray-900 dark:text-white">{{ $locations->lastItem() }}</span> of <span
-              class="font-semibold text-gray-900 dark:text-white">{{ $locations->total() }}</span>
-          </span>
-          {{ $locations->links(data: ['scrollTo' => false]) }}
-        </nav>
       </div>
     </div>
   </section>
@@ -237,17 +290,6 @@
             @endforeach
           </tbody>
         </table>
-        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 lg:w-[1150px]"
-          aria-label="Table navigation">
-          <span
-            class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-            Showing <span class="font-semibold text-gray-900 dark:text-white">{{ $measurements->firstItem() }}</span>
-            -
-            <span class="font-semibold text-gray-900 dark:text-white">{{ $measurements->lastItem() }}</span> of <span
-              class="font-semibold text-gray-900 dark:text-white">{{ $measurements->total() }}</span>
-          </span>
-          {{ $measurements->links(data: ['scrollTo' => false]) }}
-        </nav>
       </div>
     </div>
   </section>
