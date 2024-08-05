9da31f4641e986dbf7564e1b894dd0b94fc4e146
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-08-05 13:41:02 +0700

    Add button filter startdate - enddate

--- app/Livewire/Pages/Table/TableDetail.php
+++ app/Livewire/Pages/Table/TableDetail.php
@@ -9,15 +9,13 @@ use App\Models\Location;
 use App\Models\Measurement;
 use App\Models\TrainProfile;
 use App\Jobs\GenerateDetailReport;
-use Spatie\LaravelPdf\Facades\Pdf;
-use Spatie\Browsershot\Browsershot;
-use Spatie\LaravelPdf\Enums\Format;
-use Illuminate\Support\Facades\File;
 
 class TableDetail extends Component
 {
 
     public $id;
+    public $startDate;
+    public $endDate;
 
     public function mount($id)
     {
@@ -34,48 +32,11 @@ class TableDetail extends Component
         return view('livewire.pages.table.table-detail', ['train' => $train, 'device' => $device, 'locations' => $locations, 'measurements' => $measurements]);
     }
 
-    public function generatePdf()
-    {
-        $reportsDirectory = base_path('public/reports');
-
-        if (!File::exists($reportsDirectory)) {
-            File::makeDirectory($reportsDirectory, 0755, true);
-        }
-        $pdfFilePath = $reportsDirectory . "/" . "Test" . ".pdf";
-
-        $train = TrainProfile::with('stations')->where('device_id', $this->id)->first();
-        $device = Device::selectRaw('*, ST_X(last_location::geometry) AS longitude, ST_Y(last_location::geometry) AS latitude')->where('serial_number', $this->id)->first();
-        $device->last_monitored_value = json_decode($device->last_monitored_value, true);
-        $locations = Location::selectRaw('*, ST_X(point::geometry) AS longitude, ST_Y(point::geometry) AS latitude')->where('device_id', $this->id)->orderBy('created_at', 'desc')->paginate(5, pageName: 'locationsPage');
-        $measurements = Measurement::where('device_id', $this->id)->orderBy('created_at', 'desc')->paginate(5, pageName: 'measurementsPage');
-        $calculatedMeasurements = Measurement::selectRaw("key, MIN(value) as minimum, MAX(value) as maximum, AVG(value) as average, unit")
-            ->where('device_id', $this->id)
-            ->groupByRaw('key, unit')
-            ->get();
-        Pdf::view('report', [
-            "train" => $train,
-            "devices" => $device,
-            "locations" => $locations,
-            "measurements" => $measurements,
-            "calculatedMeasurements" => $calculatedMeasurements
-        ])
-            ->footerView('pdf.layout.footer')
-            ->withBrowsershot(function (Browsershot $browsershot) {
-                $browsershot
-                    ->setNodeBinary(getenv('BROWSERSHOT_NODE_PATH'))
-                    ->setNpmBinary(getenv('BROWSERSHOT_NPM_PATH'))
-                    ->setChromePath(getenv('BROWSERSHOT_CHROME_PATH'));
-            })
-            ->margins(10, 10, 10, 10)
-            ->format(Format::A4)
-            ->landscape()
-            ->save($pdfFilePath);
-    }
-
     public function createReport()
     {
         $reportId = $this->saveReport();
         GenerateDetailReport::dispatch($this->id, $reportId);
+        dd($this->startDate, $this->endDate);
     }
 
     public function saveReport()
--- resources/views/livewire/pages/table/table-detail.blade.php
+++ resources/views/livewire/pages/table/table-detail.blade.php
@@ -1,69 +1,58 @@
 <div>
   @section('title', $device->serial_number)
   <section class="lg:mt-[7rem] lg:ml-[325px]">
-    <button wire:click="createReport" type="button"
-      class="text-white bg-blue-700 
-      hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
-      font-medium rounded-lg text-sm px-5 py-2.5 
-      me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 
-      focus:outline-none dark:focus:ring-blue-800">
-      Generate Report
-    </button>
+      <!-- Modal toggle -->
+      <button data-modal-target="static-modal" data-modal-toggle="static-modal"  type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Generate Report</button>
+      <!-- Main modal -->
+      <div id="static-modal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div class="relative p-4 w-full max-w-2xl max-h-full">
+              <!-- Modal content -->
+              <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  <!-- Modal header -->
+                  <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                      <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                          Generate Report
+                      </h3>
+                      <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="static-modal">
+                          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                          </svg>
+                          <span class="sr-only">Close modal</span>
+                      </button>
+                  </div>
+                  <!-- Modal body -->
+                  <div class="flex items-center">
+                    <label for="select devices" class="block lg:mt-[-40px] lg:ml-6 text-sm font-medium 
+                            text-gray-900 dark:text-white">
+                            Filter Data By Date
+                    </label>
+                    <div class="relative mt-12 lg:ml-[-120px]">
+                      <input wire:model="startDate" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 
+                            focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 
+                            dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                            dark:focus:border-blue-500 lg:w-[200px]"
+                            type="datetime-local" name="startDate">
+                            
+                    </div>
+                    <span class="mx-4 text-gray-500 mt-12">To</span>
+                    <div class="relative mt-12">
+                      <input wire:model="endDate" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 
+                              focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 
+                              dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 
+                              dark:focus:border-blue-500 lg:w-[200px]"
+                              type="datetime-local" name="endDate">
+                    </div>
+                  </div>
+                  <!-- Modal footer -->
+                  <div class="flex items-center p-4 md:p-5 rounded-b dark:border-gray-600">
+                      <button data-modal-hide="static-modal" wire:click="createReport" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Generate</button>
+                      <button data-modal-hide="static-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cancel</button>
+                  </div>
+              </div>
+          </div>
+      </div>
   </section>
-  {{-- <section class="lg:mt-[7rem]">
-    <!-- Modal toggle -->
-    <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-      Toggle modal
-    </button>
 
-    <!-- Main modal -->
-    <div id="authentication-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative p-4 w-full max-w-md max-h-full">
-            <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <!-- Modal header -->
-                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        Sign in to our platform
-                    </h3>
-                    <button type="button" class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                </div>
-                <!-- Modal body -->
-                <div class="p-4 md:p-5">
-                    <form class="space-y-4" action="#">
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
-                        </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
-                        </div>
-                        <div class="flex justify-between">
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-                                </div>
-                                <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                            </div>
-                            <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                        </div>
-                        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                        <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered? <a href="#" class="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div> 
-
-  </section> --}}
   <section class="bg-white dark:bg-gray-900 lg:mt-[-50px]">
     <div class="pt-8 px-4 mx-auto max-w-screen-xl lg:pt-16">
       <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
@@ -237,6 +226,12 @@
             @endforeach
           </tbody>
         </table>
+        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 lg:w-[1150px]" aria-label="Table navigation">
+          <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+              Showing <span class="font-semibold text-gray-900 dark:text-white">{{ $locations->firstItem() }}</span> - <span class="font-semibold text-gray-900 dark:text-white">{{ $locations->lastItem() }}</span> of <span class="font-semibold text-gray-900 dark:text-white">{{ $locations->total() }}</span>
+          </span>
+          {{ $locations->links(data: ['scrollTo' => false]) }}
+        </nav>
       </div>
     </div>
   </section>
@@ -292,6 +287,12 @@
             @endforeach
           </tbody>
         </table>
+        <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4 lg:w-[1150px]" aria-label="Table navigation">
+          <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
+              Showing <span class="font-semibold text-gray-900 dark:text-white">{{ $measurements->firstItem() }}</span> - <span class="font-semibold text-gray-900 dark:text-white">{{ $measurements->lastItem() }}</span> of <span class="font-semibold text-gray-900 dark:text-white">{{ $measurements->total() }}</span>
+          </span>
+          {{ $measurements->links(data: ['scrollTo' => false]) }}
+      </nav>
       </div>
     </div>
   </section>
