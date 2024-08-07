100a790f26084e587a4e3b10b4dbd646464511a6
Rizky Yudistira <mrizkyyudistira@gmail.com>
2024-08-07 10:09:10 +0700

    Chnage button position in table detail

--- resources/views/livewire/pages/table/table-detail.blade.php
+++ resources/views/livewire/pages/table/table-detail.blade.php
@@ -38,7 +38,7 @@
       cursor: pointer;
     }
   </style>
-  <section class="lg:mt-[7rem] lg:ml-[390px]">
+  <section class="lg:mt-[7rem] lg:ml-[330px]">
     <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium 
             rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none 
             dark:focus:ring-blue-800" id="myBtn">
--- resources/views/livewire/pages/table/table.blade.php
+++ resources/views/livewire/pages/table/table.blade.php
@@ -37,7 +37,7 @@
         @endphp
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead class="text-xs text-white uppercase bg-[#2d2a6f] dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="p-4">
                             NO
